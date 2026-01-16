a2e1214fcda2b540c0f19bde2c298a813fbd76d0
Syed Hassam Jan <144311429+PapaGator0012@users.noreply.github.com>
2026-01-16 20:12:30 +0500

    Updated package.json and cleaned dependencies

--- components/blog-details/BlogDetailsArea.vue
+++ components/blog-details/BlogDetailsArea.vue
@@ -29,7 +29,7 @@
                           <p>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment.</p>
                           <p>I should be incapable of drawing a single stroke at the present moment; and yet I feel that never was a greater artist than now.</p>
                           <div class="postbox__img m-img mt-50 mb-45">
-                            <img src="assets/images/blog/blog1.jpg" alt="" />
+                            <img src="assets/images/blog/blog1.jpeg" alt="" />
                             <h5 class="postbox__img-caption">Image by <a href="#">@example01</a></h5>
                           </div>
                           <p>His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. "What's happened to me?" he thought. It wasn't a dream. His room, a proper human room although a little too small, lay peacefully between its four familiar walls. A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine.</p>
