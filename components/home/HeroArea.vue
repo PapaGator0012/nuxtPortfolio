70234726190b83e7eb0a36a00e8a8c13836b1474
Syed Hassam Jan <144311429+PapaGator0012@users.noreply.github.com>
2026-01-16 19:39:58 +0500

    Initial commit of my Nuxt portfolio app

---
+++ components/home/HeroArea.vue
@@ -0,0 +1,70 @@
+
+
+
+<template>
+   <section id="home" class="main-hero-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <!-- START HERO DESIGN AREA -->
+                    <div class="hero-content wow fadeInUp text-center delay-0-2s">
+                        <h2>Mudasar MD</h2>
+                    </div>
+                    <!-- / END HERO DESIGN AREA -->
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 pt-30">
+                    <!-- START HERO DESIGN AREA -->
+                    <div class="hero-content wow fadeInUp delay-0-2s">
+                       <div class="clienti-reviews">
+    <ul class="clienti-profile">
+        <li>
+            <a href="#testimonials">
+                <img class="img-fluid" src="assets/images/avatar/01.jpeg" alt="client">
+            </a>
+        </li>
+        <li>
+            <a href="#testimonials">
+                <img class="img-fluid" src="assets/images/avatar/02.jpeg" alt="client">
+            </a>
+        </li>
+        <li>
+            <a href="#testimonials">
+                <img class="img-fluid" src="assets/images/avatar/03.jpeg" alt="client">
+            </a>
+        </li>
+        <li>
+            <a href="#testimonials">
+                <img class="img-fluid" src="assets/images/avatar/04.jpeg" alt="client">
+            </a>
+        </li>
+    </ul>
+    <div class="reviews">
+        100+ reviews <span>(4.96 of 5)</span>
+        <p>Five-star reviews from my esteemed clients.</p>
+    </div>
+</div>
+
+                    </div>
+                    <!-- / END HERO DESIGN AREA -->
+                </div>
+                <div class="col-lg-6">
+                    <div class="hero-image">
+                        <img src="assets/images/about/me.jpg" alt="">
+                    </div>
+                    <!-- / END HERO DESIGN AREA -->
+                </div>
+                <div class="col-lg-3 pt-30">
+                    <div class="hero-content wow fadeInUp delay-0-4s">
+                        <p>Hi, I’m Muddassar, a passionate UX Designer dedicated to creating user-friendly digital experiences.</p>
+                        <a class="theme-btn" href="#">Book free consultation </a>
+                    </div>
+                    <!-- / END HERO DESIGN AREA -->
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+ 
---
+++ layouts/Header.vue
@@ -0,0 +1,78 @@
+
+
+<script setup>
+
+// menu sticky 
+const isSticky = ref(false);
+const handleSticky = () => {
+  if (window.scrollY > 80) {
+    isSticky.value = true;
+  } else {
+    isSticky.value = false;
+  }
+};
+
+onMounted(() => {
+  window.addEventListener('scroll', handleSticky);
+});
+onUnmounted(() => {
+  window.removeEventListener('scroll', handleSticky);
+});
+
+
+const openMenu = ref(false)
+const toggleMenu = () => {
+  openMenu.value = !openMenu.value
+}
+
+
+import menu_data from "../data/menu_data"
+
+</script>
+
+
+<template>
+
+<div>
+    <header :class="`main-header ${isSticky ? 'fixed-header' : ''}`">
+        <div class="header-upper">
+            <div class="container">
+                <div class="header-inner">
+                    <div class="row align-items-center">
+                        <div class="col-xl-2 col-lg-2 col-md-6 col-6 col-sm-3">
+                            <div class="logo-area">
+                                <div class="logo">
+                                    <NuxtLink to="/"><img src="assets/images/logo.png" alt=""></NuxtLink>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-10 col-lg-10 col-md-6 col-6 col-sm-9">
+                          <div class="main-menu d-none d-lg-block">
+                            <nav id="mobile-menu">
+                                <ul>
+                                    <!-- Manually add the "Contact" button here -->
+                                    <li>
+                                        <NuxtLink to="/contact" class="linkstyle">Contact MD</NuxtLink>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+
+                            <div class="side-menu-icon d-lg-none text-end">
+                                <a style="cursor: pointer;" 
+                                @click="toggleMenu"
+                                class="info-toggle-btn f-right sidebar-toggle-btn"><i class="fal fa-bars"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <CommonSidebar :toggleMenu="toggleMenu" :openMenu="openMenu"  />
+</div>
+  
+
+</template>
+
